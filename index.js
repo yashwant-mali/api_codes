729e721dee19222339dc10c9e14d7fdfa2e99247
yashwant-mali <yashwantmali555@gmail.com>
2024-10-01 22:17:18 +0530

    starting to commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require(express);
+const app=express();
+
+const user=[
+    {id:1,name:'yashwant mali', email:'yashwantmali555@gmail.com'},
+    {id:2,name:'vaishnavi mali', email:'vaishnavimali555@gmail.com'},
+    {id:3,name:'tejaswini mali', email:'tejaswinimali555@gmail.com'}
+];
+
+app.get()
